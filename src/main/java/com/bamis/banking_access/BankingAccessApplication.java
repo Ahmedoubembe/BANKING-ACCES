386b3a17a4d091f46143caef9638ab03c198538d
Ahmedoubembe <22024@supnum.mr>
2026-02-11 14:15:31 +0000

    first commit acces_banking

---
+++ src/main/java/com/bamis/banking_access/BankingAccessApplication.java
@@ -0,0 +1,13 @@
+package com.bamis.banking_access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAccessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingAccessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bamis/banking_access/BankingAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamis.banking_access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAccessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
